4728fe909b4095eb811467020a536d44c23fc944
Wijnand <wijnandvdmeijs@gmail.com>
2024-05-18 03:44:45 -0400

    both selects are now views with proper names

--- v_per_ingredient.sql
+++ v_per_ingredient.sql
@@ -1,7 +1,8 @@
--- CREATE VIEW v_ingredient_nutrition AS
+CREATE VIEW v_per_ingredient AS
 SELECT 
     mp.meal_prep_name,
-	SUM(CASE WHEN c.component_name = 'calories' THEN (ic.component_quantity * mi.ingredient_quantity) / 100 ELSE 0 END) AS total_calories,
+    i.ingredient_name,
+    SUM(CASE WHEN c.component_name = 'calories' THEN (ic.component_quantity * mi.ingredient_quantity) / 100 ELSE 0 END) AS total_calories,
     SUM(CASE WHEN c.component_name = 'protein' THEN (ic.component_quantity * mi.ingredient_quantity) / 100 ELSE 0 END) AS total_protein
 FROM 
     raw.ingredients i
@@ -10,4 +11,5 @@ JOIN raw.meal_preps mp ON mi.meal_prep_id = mp.meal_prep_id
 JOIN raw.ingredients_components ic ON i.ingredient_id = ic.ingredient_id
 JOIN raw.components c ON ic.component_id = c.component_id
 GROUP BY 
-    mp.meal_prep_name;
+    mp.meal_prep_name, 
+    i.ingredient_name;
