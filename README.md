4868ba07cd98dbf01c7a91c703f18d5541730fa0
Wijnand <wijnandvdmeijs@gmail.com>
2024-09-04 01:48:25 -0400

    added commands for creating and using cloud resources

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 # recipes-db
 
+
+## for local development
 # build docker image
-docker build -t flask-yaml-processor .
+docker build -t recipe-calculator .
 
 # run docker image
-docker run -p 5000:5000 flask-yaml-processor
+docker run -p 5000:5000 recipe-calculator
+
+## for pushing to azure
+# build docker image
+docker build -t acrhonestdatasolutions.azurecr.io/recipe-calculator .
+
+# push to azure
+docker push acrhonestdatasolutions.azurecr.io/recipe-calculator
+
+# create container instance from azure container registry
+az container create -g MyResourceGroup --name myapp --image myAcrRegistry.azurecr.io/myimage:latest --registry-password password
+
+# inspiration
+https://learn.microsoft.com/en-us/azure/developer/python/tutorial-containerize-simple-web-app?tabs=web-app-flask
---
+++ azure-infrastructure/azure-cli-create.sh
@@ -0,0 +1,17 @@
+resource_group="rg-containers"
+location="westeurope"
+container_registry="acrhonestdatasolutions"
+container_registry_sku="Basic"
+
+image="recipe-calculator"
+container_instance="ci-recipe-calculator"
+
+## basic infrastructure
+# az group create --name ${resource_group} --location ${location}
+# az acr create --resource-group ${resource_group} --name ${container_registry} --sku ${container_registry_sku}
+
+# for docker part, see README.md
+acr_password=$(az acr credential show -n ${container_registry} | jq -r .passwords[0].value)
+
+az container create -g ${resource_group} --name ${container_instance} --ip-address Public --image "${container_registry}.azurecr.io/${image}" --registry-password ${acr_password} --registry-username ${container_registry} --ports 5000 80
+
