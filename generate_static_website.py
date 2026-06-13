313b2a5cf2c9f09c61b86bc52d9b5194b13a33f3
Wijnand van der Meijs <wijnandvdmeijs@gmail.com>
2026-06-13 17:25:13 +0200

    added clock icon

--- generate_static_website.py
+++ generate_static_website.py
@@ -204,6 +204,7 @@ def process_all_recipes(
                             "steps": yaml_content.get("steps", []),
                             "rating": yaml_content.get("rating", 0),
                             "spice_level": yaml_content.get("spice_level", 0),
+                            "preparation_time": yaml_content.get("preparation_time"),
                             "filename": filename.replace(".yaml", ".html"),
                             "dietary_labels": yaml_content.get("dietary_labels", []),
                             "category": category.title(),
